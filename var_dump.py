d072f019753e4eeaf858af5c2be452dfd86b907f
h2809 <y0SHi@softbank.ne.jp>
2016-11-15 10:49:49 +0000

    add shebang

--- var_dump.py
+++ var_dump.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 def var_dump(var, prefix=''):
     if type(var) in (int, float, str, complex):
         print(prefix, '(', var.__class__.__name__, ') ', var, sep='')
@@ -13,4 +16,3 @@ def var_dump(var, prefix=''):
                     print(prefix, i, ': (', var[i].__class__.__name__, ') ', var[i], sep='')
                 else:
                     print(prefix, '(', i.__class__.__name__, ') ', i, sep='')
-
